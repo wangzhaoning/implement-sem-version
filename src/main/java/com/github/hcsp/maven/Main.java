df90d70f28137d4fcb35d9c729cb202fa8dce3c9
Jinlf <2354833@qq.com>
2019-07-18 10:59:31 +0800

    实现一个语义化版本

--- src/main/java/com/github/hcsp/maven/Main.java
+++
@@ -1,8 +0,0 @@
-package com.github.hcsp.maven;
-
-public class Main {
-    public static void main(String[] args) {
-        Version v = Version.of("1.0");
-        System.out.println(v.getMajorVersion()+"."+v.getMinorVersion()+"."+v.getPatchVersion());
-    }
-}
