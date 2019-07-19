59c714addfbb447d775f77d1b41dc85c25ab1a1c
zyw <40329182+zhangyongwang4444@users.noreply.github.com>
2019-07-19 18:39:17 +0800

    implement-sem-version (#7)
    implement-sem-version (#7)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.maven;
 
-public class Version {
+class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
      *
@@ -13,5 +13,38 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    static int compare(String version1, String version2) {
+
+        int[] params1 = Version.handleString(version1);
+        int[] params2 = Version.handleString(version2);
+        return Version.compareTo(params1, params2);
+
+    }
+
+    private static int[] handleString(String version) {
+        int[] v = new int[]{0, 0, 0};
+        String[] strArray = version.split("\\.");
+
+        for (int i = 0; i < strArray.length; i++) {
+            v[i] = Integer.parseInt(strArray[i]);
+        }
+        return v;
+    }
+
+    private static int compareTo(int[] a, int[] b) {
+
+        int[] result = new int[]{0, 0, 0};
+        for (int i = 0; i < 3; i++) {
+            result[i] = a[i] - b[i];
+        }
+
+        if (result[0] == 0 && result[1] == 0 && result[2] == 0) {
+            return 0;
+        } else if (result[0] > 0 || (result[0] >= 0 && result[1] > 0) || (result[0] >= 0 && result[1] >= 0 && result[2] > 0)) {
+            return 1;
+        } else {
+            return -1;
+        }
+
+    }
 }
