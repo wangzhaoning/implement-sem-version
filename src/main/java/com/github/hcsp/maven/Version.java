66fb415e0ee07dbfeaf70f098700a09c2db7f1ec
JayChenFE <chenjiehwy@163.com>
2019-07-23 12:16:24 +0800

    自己实现语义版本检查

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import java.util.Arrays;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,30 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        int[] version1Arr = toIntArray(version1);
+        int[] version2Arr = toIntArray(version2);
+        int maxLength = Math.max(version1Arr.length, version2Arr.length);
+        for (int i = 0; i < maxLength; i++) {
+            int item1 = getVersionItem(version1Arr, i);
+            int item2 = getVersionItem(version2Arr, i);
+            if (item1 == item2) {
+                continue;
+            }
+            return item1 < item2 ? -1 : 1;
+        }
+        return 0;
+    }
+
+    private static int[] toIntArray(String version) {
+        return Arrays.stream(version.split("\\.")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static int getVersionItem(int[] array, int index) {
+        if (index > array.length - 1) {
+            return 0;
+        }
+        return array[index];
+
+    }
 }
