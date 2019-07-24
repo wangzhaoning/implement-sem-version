a849326c358f3bc7ef5bb0e21276b1c0f6479ede
catsme <845058547@qq.com>
2019-07-24 10:20:02 +0800

    实现语义化版本比较.

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Version {
+class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
      *
@@ -17,35 +17,30 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {
-        List<String> version1AsList = Arrays.asList(version1.split("\\."));
-        ArrayList<String> newVersion1AsList = new ArrayList<>(version1AsList);
-        List<String> version2AsList = Arrays.asList(version2.split("\\."));
-        ArrayList<String> newVersion2AsList = new ArrayList(version2AsList);
+    static int compare(String version1, String version2) {
+        List<String> newVersion1AsList = new ArrayList<>(Arrays.asList(version1.split("\\.")));
+        List<String> newVersion2AsList = new ArrayList<>(Arrays.asList(version2.split("\\.")));
         int largeVersionNumber = Math.max(newVersion1AsList.size(), newVersion2AsList.size());
-        if (newVersion1AsList.size() < largeVersionNumber) {
-            formatVersion(newVersion1AsList, largeVersionNumber);
-        } else formatVersion(newVersion2AsList, largeVersionNumber);
+        if (newVersion1AsList.size() < largeVersionNumber) formatVersion(newVersion1AsList, largeVersionNumber);
+        else {
+            formatVersion(newVersion2AsList, largeVersionNumber);
+        }
 
         for (int i = 0; i < largeVersionNumber; i++) {
             if (Integer.parseInt(newVersion1AsList.get(i)) > Integer.parseInt(newVersion2AsList.get(i))) {
                 return 1;
-            } else if (Integer.parseInt(newVersion1AsList.get(i)) < Integer.parseInt(newVersion2AsList.get(i))) {
-                return -1;
+            } else {
+                if (Integer.parseInt(newVersion1AsList.get(i)) < Integer.parseInt(newVersion2AsList.get(i))) {
+                    return -1;
+                }
             }
         }
         return 0;
     }
 
-    private static void formatVersion(List<String> version, int Number) {
-        for (int i = version.size(); i < Number; i++) {
-            version.add(String.valueOf(0));
+    private static void formatVersion(List<String> version, int number) {
+        for (int i = version.size(); i < number; i++) {
+            version.add("0");
         }
-    }
-
-    public static void main(String[] args) {
-        compare("1.0", "2.2");
-        compare("1.1", "2.2.1");
-        compare("1", "2.2.2");
     }
 }
