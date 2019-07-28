19e3df1702d7bc5f9eaae7a3ff209bb211912b4b
Moon <justwe77@163.com>
2019-07-28 13:08:34 +0800

    modify

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.maven;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +17,27 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static final int VERSION_LENGTH = 3;
+    public static int compare(String version1, String version2) {
+        List<String> v1 = new ArrayList<>(Arrays.asList(version1.split("\\.")));
+        List<String> v2 = new ArrayList<>(Arrays.asList(version2.split("\\.")));
+        toStandardFormat(v1);
+        toStandardFormat(v2);
+        for (int i = 0; i < VERSION_LENGTH; i++) {
+            if (Integer.parseInt(v1.get(i)) < Integer.parseInt(v2.get(i))){
+                return -1;
+            } else if (Integer.parseInt(v1.get(i)) > Integer.parseInt(v2.get(i))){
+                return 1;
+            } else {
+                continue;
+            }
+        }
+        return 0;
+    }
+    public static void toStandardFormat(List<String> original){
+        int original_size = original.size();
+        for (int i = original_size; i < VERSION_LENGTH; i++) {
+            original.add("0");
+        }
+    }
 }
