6daa305161da1045f44d96d5c7f5cc4dea4f4bf2
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-07-29 20:13:32 +0800

    Update Version.java (#14)
    Update Version.java (#14)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,25 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        int[] v1 = 补零(version1);
+        int[] v2 = 补零(version2);
+        for (int i = 0; i < 3; i++){
+            if (v1[i] > v2[i]){
+                return 1;
+            }else if (v1[i] < v2[i]){
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    private static int[] 补零(String version){
+        int[] result = new int[3];
+        int i = 0;
+        for (String temp : version.split("\\.")){
+            result[i++] = Integer.valueOf(temp);
+        }
+        return result;
+    }
 }
