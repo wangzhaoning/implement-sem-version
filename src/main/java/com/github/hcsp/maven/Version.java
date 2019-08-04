2db260c149d44409ca4506f45c31839b3584da73
XiongAmao <onycat@live.cn>
2019-08-04 19:56:37 +0800

    Implement semantic version.

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import java.util.ArrayList;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,34 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        ArrayList<Integer> v1List = splitVersionStringToIntList(version1);
+        ArrayList<Integer> v2List = splitVersionStringToIntList(version2);
+        int result = 0;
+
+        for (int i = 0; i < 3; i++) {
+            result = v1List.get(i).compareTo(v2List.get(i));
+
+            if (result != 0) {
+                return result;
+            }
+        }
+
+        return result;
+    }
+
+    public static ArrayList<Integer> splitVersionStringToIntList(String version) {
+        String[] temp = version.split("\\.");
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            if (i < temp.length) {
+                result.add(Integer.valueOf(temp[i]));
+            } else {
+                result.add(0);
+            }
+        }
+
+        return result;
+    }
 }
