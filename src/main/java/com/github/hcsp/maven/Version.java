7608785f4c71a487e03dd824078da27ee4273736
Mingru Li <limingru1991@gmail.com>
2019-08-15 14:56:32 +0800

    语义化版本比较 (#23)
    语义化版本比较 (#23)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,27 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        String[] arr1 = version1.split("\\.");
+        String[] arr2 = version2.split("\\.");
+        int length = Math.max(arr1.length, arr2.length);
+        for (int i = 0; i < length; ++i) {
+            int num1 = i < arr1.length ? Integer.parseInt(arr1[i]) : 0;
+            int num2 = i < arr2.length ? Integer.parseInt(arr2[i]) : 0;
+
+            if (num1 < num2) {
+                return -1;
+            } else if (num1 > num2) {
+                return 1;
+            }
+        }
+
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compare("1", "1.0"));
+        System.out.println(compare("1.0.19", "1.0.1"));
+    }
 }
