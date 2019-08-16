a13feb77f7044dde6adec1d76761c6f9b9a5d758
tongkang <tongkang@gmail.com>
2019-08-16 16:54:56 +0800

    语义化版本比较

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,12 @@
 package com.github.hcsp.maven;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.lang.Integer.parseInt;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +20,41 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+
+
+
+        ArrayList<String> version1_arr = new ArrayList<>(Arrays.asList(version1.split("\\.")));
+        ArrayList<String> version2_arr = new ArrayList<>(Arrays.asList(version2.split("\\.")));
+
+        int len = Math.max(version1_arr.size(), version2_arr.size());
+
+        while (version1_arr.size()<len){
+            version1_arr.add("0");
+        }
+
+        while (version2_arr.size()<len){
+            version2_arr.add("0");
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (Integer.parseInt(version1_arr.get(i))<Integer.parseInt(version2_arr.get(i)))
+                return -1;
+
+            if (Integer.parseInt(version1_arr.get(i))>Integer.parseInt(version2_arr.get(i)))
+                return 1;
+        }
+
+        return 0;
+
+    }
+
+//    @Test
+//    public void test(){
+//        System.out.println(compare("1.2.","1.2.3"));
+//        System.out.println(compare("1.0.0","1"));
+//        System.out.println(compare("0.0.1","0.0"));
+//
+//    }
+
 }
