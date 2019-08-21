0ac790f69113d0a866161f93ba2f8808384c3b51
胡凯波 <1069977596@qq.com>
2019-08-21 12:27:00 +0800

    改进改进

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -17,16 +17,7 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static void main(String[] args) {
-        compare("0.1.2", "2.1.2");
-    }
-    private static List<String> difficultVersion(String version) {
-        List<String> data = new ArrayList<>(Arrays.asList(version.split("\\.")));
-        while (data.size() < 3) {
-            data.add("0");
-        }
-        return data;
-    }
+
     public static int compare(String version1, String version2) {
         for (int i = 0; i < 3; i++) {
             int v1 = Integer.parseInt(difficultVersion(version1).get(i));
@@ -43,5 +34,15 @@ public class Version {
         }
         System.out.println(version1 + "=" + version2);
         return 0;
+    }
+    public static void main(String[] args) {
+        compare("2.2.1", "2.1.2");
+    }
+    private static List<String> difficultVersion(String version) {
+        List<String> data = new ArrayList<>(Arrays.asList(version.split("\\.")));
+        while (data.size() < 3) {
+            data.add("0");
+        }
+        return data;
     }
 }
