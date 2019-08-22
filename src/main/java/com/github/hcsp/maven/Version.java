4d8682313ef8100cdcb37865ec507149db1d54f1
Suyu <zhuangsuyu1993@163.com>
2019-08-22 13:39:05 +0800

    实现语义化版本 (#29)
    实现语义化版本 (#29)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.maven;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +17,33 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        List<String> v1Arr = processArr(version1);
+        List<String> v2Arr = processArr(version2);
+        return doCompare(v1Arr, v2Arr);
+    }
+
+    private static int doCompare(List<String> v1Arr, List<String> v2Arr) {
+        for (int i = 0; i < 3; i++) {
+            if (Integer.parseInt(v1Arr.get(i)) > Integer.parseInt(v2Arr.get(i))) {
+                return 1;
+            } else if (Integer.parseInt(v1Arr.get(i)) < Integer.parseInt(v2Arr.get(i))) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    private static List<String> processArr(String version) {
+        List<String> arr = new ArrayList<>(Arrays.asList(version.split("\\.")));
+        if (arr.size() == 1) {
+            arr.add("0");
+            arr.add("0");
+        } else if (arr.size() == 2) {
+            arr.add("0");
+        }
+        return arr;
+    }
+
+
 }
