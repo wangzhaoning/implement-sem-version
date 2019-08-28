51bba1854c36ea5cde47160cde6a976e4a769763
kungua <52815012+kungua@users.noreply.github.com>
2019-08-28 22:32:41 +0800

    implement sem version (#33)
    implement sem version (#33)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.maven;
 
+import org.apache.maven.artifact.versioning.ComparableVersion;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +15,10 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        ComparableVersion comparableVersion1 = new ComparableVersion(version1);
+        ComparableVersion comparableVersion2 = new ComparableVersion(version2);
+        return comparableVersion1.compareTo(comparableVersion2);
+    }
+
 }
