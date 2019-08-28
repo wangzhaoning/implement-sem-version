f0ac75bca79080412329edd1f24d3fd00c5de859
kungua <52815012+kungua@users.noreply.github.com>
2019-08-28 22:04:02 +0800

    implement sem version (#32)
    implement sem version (#32)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -13,5 +13,45 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        String[] version1StringList = version1.split("\\.");
+        String[] version2StringList = version2.split("\\.");
+        String[] longerList, shorterList;
+        int longerListFlag = 1;
+        if (version1StringList.length > version2StringList.length) {
+            longerList = version1StringList;
+            shorterList = version2StringList;
+        } else {
+            longerListFlag = 2;
+            longerList = version2StringList;
+            shorterList = version1StringList;
+        }
+        for (int i = 0; i < shorterList.length; i++) {
+            int item1 = Integer.parseInt(version1StringList[i]);
+            int item2 = Integer.parseInt(version2StringList[i]);
+            if (item1 < item2) {
+                return -1;
+            } else if (item1 > item2) {
+                return 1;
+            }
+        }
+        if (shorterList.length == longerList.length) {
+            return 0;
+        }
+        for (int i = shorterList.length; i < longerList.length; i++) {
+            int item = Integer.parseInt(longerList[i]);
+            if (item != 0) {
+                if (longerListFlag == 1) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        compare("1.0", "1");
+    }
 }
