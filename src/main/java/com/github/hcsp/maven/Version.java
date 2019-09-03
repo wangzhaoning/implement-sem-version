cdf307a6f624fd28f2b2f0e13868bc49372a3100
EthanWXL <46627960+EthanWXL@users.noreply.github.com>
2019-09-03 14:33:45 +0800

    实现语义化版本 (#36)
    实现语义化版本 (#36)

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,17 +1,42 @@
 package com.github.hcsp.maven;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
-     *
+     * <p>
      * <p>传入两个形如x.y.z的字符串，比较两个语义化版本的大小。如果version1小于version2，返回-1；如果version1大于
      * version2，返回1。如果二者相等，返回0。
-     *
+     * <p>
      * <p>注意，如果传入的字符串形如x，则其等价于x.0.0。 如果传入的字符串形如x.y，则其等价于x.y.0。
      *
      * @param version1 传入的版本字符串1，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+
+    public static int compare(String version1, String version2) {
+        for (int i = 0; i < 3; i++) {
+            int v1 = Integer.parseInt(stylizeVersion(version1).get(i));
+            int v2 = Integer.parseInt(stylizeVersion(version2).get(i));
+            if (v1 < v2) {
+                return -1;
+            } else if (v1 > v2) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    private static List<String> stylizeVersion(String version) {
+        List<String> vList = new ArrayList<>(Arrays.asList(version.split("\\.")));
+        while (vList.size() < 3) {
+            vList.add("0");
+        }
+        return vList;
+    }
+
 }
