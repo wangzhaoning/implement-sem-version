fbe48b87dbea7831490c09e532e8ec9e285d2c75
wangzhaoning <wangzhaoning@inspur.com>
2019-09-18 16:12:40 +0800

    add:test pass

--- src/main/java/com/github/hcsp/maven/Version.java
+++ src/main/java/com/github/hcsp/maven/Version.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.maven;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Version {
     /**
      * 请根据语义化版本的要求 https://semver.org/lang/zh-CN/ ，比较两个"语义化版本"
@@ -13,5 +17,31 @@ public class Version {
      * @param version2 传入的版本字符串2，支持x/x.y/x.y.z，你可以假定传入的字符串一定是合法的语义化版本
      * @return -1/0/1 当version1 小于/等于/大于 version2时
      */
-    public static int compare(String version1, String version2) {}
+    public static int compare(String version1, String version2) {
+        if(version1.equals(version2)) return 0;
+        List<Integer> v1=new ArrayList<>();
+        List<Integer> v2=new ArrayList<>();
+        StringTokenizer stv1=new StringTokenizer(version1,".");
+        while(stv1.hasMoreTokens()) {
+            v1.add(Integer.parseInt(stv1.nextToken()));
+        }
+        StringTokenizer stv2=new StringTokenizer(version2,".");
+        while(stv2.hasMoreTokens()) {
+            v2.add(Integer.parseInt(stv2.nextToken()));
+        }
+        convert(v1);
+        convert(v2);
+        for(int i=0;i<v1.size();i++){
+            if(v1.get(i)>v2.get(i))return 1;
+            else if(v1.get(i)<v2.get(i)) return -1;
+        }
+        return 0;
+    }
+
+    private static void convert(List<Integer> v) {
+        if (v.size()<3){
+            v.add(0);
+            convert(v);
+        }
+    }
 }
